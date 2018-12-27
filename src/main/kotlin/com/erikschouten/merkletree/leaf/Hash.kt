0d3b493fe8613e7cebdcec5f0c8cf494cf900073
Erik Schouten <Erik@ErikSchouten.com>
2018-12-27 13:31:42 +0100

    Public constructor for string

--- src/main/kotlin/com/erikschouten/merkletree/leaf/Hash.kt
+++ src/main/kotlin/com/erikschouten/merkletree/leaf/Hash.kt
@@ -3,31 +3,10 @@ package com.erikschouten.merkletree.leaf
 import com.erikschouten.merkletree.Merkle
 import org.bouncycastle.util.encoders.Base64
 
-class Hash private constructor(val value: String) : Merkle {
+data class Hash constructor(val value: String) : Merkle {
 
     constructor(bytes: ByteArray) : this(Base64.toBase64String(bytes))
     constructor(merkle: Merkle) : this(merkle.sha3())
 
     override fun sha3() = Base64.decode(value)!!
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as Hash
-
-        if (value != other.value) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        return value.hashCode()
-    }
-
-    override fun toString(): String {
-        return "Hash(value='$value')"
-    }
-
-
 }
